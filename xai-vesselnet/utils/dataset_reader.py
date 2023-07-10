eb1de682a5daab79dcfbbcabeab9cc4f1775590e
Guillaume Garret <Guillaume.GARRET@uca.fr>
2023-07-10 14:04:21 +0200

    Add parse function for CSV dataset

---
+++ xai-vesselnet/utils/dataset_reader.py
@@ -0,0 +1,14 @@
+import csv
+
+def parse_dataset_csv(csv_path, delimiter=';'):
+    x = []
+    y = []
+
+    with open(csv_path, mode='r') as csv_file:
+        dataset_reader = csv.reader(csv_file, delimiter=delimiter)
+
+        for line in dataset_reader:
+            x.append(line[0])
+            y.append(line[1])
+
+    return x, y
