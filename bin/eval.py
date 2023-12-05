a9bd00f00aa65da6baa870c64532eccf06500720
Guillaume Garret <Guillaume.GARRET@uca.fr>
2023-12-05 17:23:51 +0100

    Improvements to the evaluation process  * eval.py now uses infer.py  * The sample JSON scheduler has been updated to meet the minimum requirements for evaluation. Note that the 'mask' field must be present, even if empty. TODO: catch the key error exception to be more flexible.

--- bin/eval.py
+++ bin/eval.py
@@ -1,22 +1,21 @@
 import argparse
 import logging
 import os
+import json
+
 
 import torch
+
 import monai
-from monai.data import DataLoader
-from monai.inferers import SlidingWindowInferer
-from monai.data.utils import decollate_batch, first
 from monai.transforms import (
-    Compose,
-    Activations,
     AsDiscrete,
-    RemoveSmallObjects,
-    SaveImage,
     LoadImage
 )
 from monai.metrics import DiceMetric
 
+from metrics.cldice import clDiceMetric
+
+import infer
 import models.instanciate_model
 
 """
@@ -37,7 +36,7 @@ def parse_arguments():
         metavar=("MODEL"),
         choices=models.instanciate_model._all_models,
         default=models.instanciate_model._all_models[0],
-        help="Path to the training CSV",
+        help="Name of the model",
     )
     parser.add_argument(
         "weights",
@@ -46,18 +45,10 @@ def parse_arguments():
         help="Path to the model's weights",
     )
     parser.add_argument(
-        "csv_eval",
+        "schedule",
         type=str,
-        metavar=("EVAL_CSV_PATH"),
-        help="Path to the evaluation CSV",
-    )
-    parser.add_argument(
-        "--mask",
-        "-m",
-        type=str,
-        metavar=("MASK_CSV_PATH"),
-        default=None,
-        help="Path to the masks CSV",
+        metavar=("JSON_SCHEDULING_PATH"),
+        help="Path to the JSON file for the evaluation scheduling",
     )
 
     parser.add_argument("--verbose", "-v", action="count", default=0)
@@ -66,110 +57,83 @@ def parse_arguments():
     return args
 
 
-def main():
-    from utils.dataset_reader import parse_csv
-    from utils.logging_blocks import log_hardware
-    from models.instanciate_model import instanciate_model
-    from datasets.instanciate_dataset import instanciate_image_dataset
+def parse_eval_scheduling_json_file(json_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    log_hardware(device)
+    return data
 
-    infer_ds = instanciate_image_dataset(args.csv_eval, image_only=False)
-    infer_loader = DataLoader(infer_ds, batch_size=1, num_workers=0) # Batch_size must be equal to 1 as input tensors does not have equal spatial dims. See sw_batch_size for parallelization.
 
-    saver = SaveImage(
-        output_dir=cfg.result_dir,
-        output_ext=".nii",
-        output_postfix=f"seg_{model_id}",
-        resample=False,
-        separate_folder=False,
-    )
+def main(model, weights, json_scheduler):
+    eval_schedule = parse_eval_scheduling_json_file(json_scheduler)
 
-    # Post-processing
-    post_transforms = Compose(
-        [Activations(sigmoid=True), AsDiscrete(threshold=0.5), RemoveSmallObjects()]
-    )
+    infer.infer(model, weights, eval_schedule) # infer data and update prediction field 
+    with open(json_scheduler, 'w') as f:
+        json.dump(eval_schedule, f)
 
-    # Model
-    in_channels = first(infer_loader)["img"].shape[1]  # We suppose the same input channels through the whole dataset
+    y_true_paths = [elem["reference"] for elem in eval_schedule["schedule"]]
+    y_pred_paths = [elem["prediction"] for elem in eval_schedule["schedule"]]
 
-    model = instanciate_model(args.model, in_channels=in_channels).to(device)
-    model.load_state_dict(torch.load(args.weights)["model_state_dict"])
-    model.eval()
+    if len(y_true_paths) != len(y_pred_paths):
+        raise AssertionError("The number of references and prediction must be equal.")
+    
+    for k in range(len(y_true_paths)):
+        logger.info(f"Evaluate sample {y_true_paths[k]}")
 
-    # Inference parameters
-    sw_shape = (64, 64, 64)  # TODO: Read from the --hyperparameters
-    sw_batch_size = 64  # TODO: Move into a cfg file
-    sw_overlap = 0.25  # TODO: Move into a cfg file
+        y_true = LoadImage(image_only=True, ensure_channel_first=True)(y_true_paths[k])
+        y_pred = LoadImage(image_only=True, ensure_channel_first=True)(y_pred_paths[k])
 
-    inferer = SlidingWindowInferer(
-        sw_shape,
-        sw_batch_size=sw_batch_size,
-        overlap=sw_overlap
-    )
+        ohe_true = torch.unsqueeze(
+            monai.networks.utils.one_hot(y_true, num_classes=2, dim=0),
+            axis=0
+        )
+        logger.debug(f"{y_true_paths[k]} : encode to one-hot... {ohe_true.shape}")
 
-    metrics = [
-        # The metrics needs batch dimension (N, C, H, W, [D]) and one-hot-encoding (MONAI v1.1)
-        DiceMetric(include_background=False, reduction="mean")
-    ]
+        ohe_pred = torch.unsqueeze(
+            monai.networks.utils.one_hot(y_pred, num_classes=2, dim=0),
+            axis=0
+        )
+        logger.debug(f"{y_pred_paths[k]} : encode to one-hot... {ohe_pred.shape}")
 
-    if args.mask is not None:
-        masks_arr = parse_csv(args.mask)
-        assert len(infer_loader) == len(masks_arr) # One line of per evaluation volume. Multiple columns per line for multiple masks
-    else:
-        masks_arr = []
+        # TODO : Gérer les métriques en fonction de ce qui est décrit dans le JSON
 
-    for i, batch in enumerate(infer_loader):
-        xs, ys, meta = batch["img"].to(device), batch["seg"].to(device), batch["img_meta"]
+        metrics = [
+            # The metrics needs batch dimension (N, C, H, W, [D]) and one-hot-encoding (MONAI v1.1)
+            DiceMetric(include_background=False, reduction="mean"),
+            clDiceMetric(include_background=False, reduction="mean")
+            #SurfaceDistanceMetric(include_background=False, symmetric=True, distance_metric="euclidean")
+        ]
 
-        predictions = inferer(inputs=xs, network=model)
-        predictions = [post_transforms(i) for i in decollate_batch(predictions)]
+        # Classic global metrics
+        logger.info("Global :")
+        for metric in metrics:
+            metric(y_pred=ohe_pred, y=ohe_true)
+            logger.info("\t- {} = {}".format(type(metric).__name__, metric.aggregate().item()))
+            metric.reset()
 
-        ys = [y for y in decollate_batch(ys)]
+        # Apply mask if exists
+        masks = eval_schedule["schedule"][k]["mask"]
 
-        for j, prediction in enumerate(predictions):
-            logger.info(f"Validation: {meta['filename_or_obj'][j]}")
+        for mask in masks:
+            mask_data = LoadImage(image_only=True, ensure_channel_first=True)(mask)            
 
-            saver(prediction)
-
-            ohe_pred = torch.unsqueeze(
-                monai.networks.utils.one_hot(prediction, num_classes=2, dim=0),
+            # Prepare the mask : 1/ binarize 2/ one-hot encoding 3/ add the batch channel
+            mask_data = AsDiscrete(threshold=0.5)(mask_data)
+            ohe_mask = torch.unsqueeze(
+                monai.networks.utils.one_hot(mask_data, num_classes=2, dim=0),
                 axis=0
             )
-            ohe_y = torch.unsqueeze(
-                monai.networks.utils.one_hot(ys[j], num_classes=2, dim=0),
-                axis=0
-            )
+            logger.debug(f"{mask} : encode to one-hot... {ohe_mask.shape}")
 
+            masked_ref = ohe_true * ohe_mask
+            masked_pred = ohe_pred * ohe_mask
+
+            # Metrics on partitions
+            logger.info(f"Mask : {mask}")
             for metric in metrics:
-                metric(y_pred=ohe_pred, y=ohe_y)
+                metric(y_pred=masked_ref, y=masked_pred)
                 logger.info("\t- {} = {}".format(type(metric).__name__, metric.aggregate().item()))
                 metric.reset()
-
-            # If mask has been provided, for each mask
-            if masks_arr:
-                for mask in masks_arr[i * infer_loader.batch_size + j]:
-                    logger.info(os.path.basename(mask).split("_")[1])
-
-                    mask = torch.unsqueeze(
-                        LoadImage(ensure_channel_first=True, image_only=True)(mask)>0, axis=0
-                    ).to(device)
-
-                    # Convert to one-hot format, to generalized use amoung metrics and add the batch dimension
-                    ohe_masked_pred = torch.unsqueeze(
-                        monai.networks.utils.one_hot(prediction*mask, num_classes=2, dim=0),
-                        axis=0
-                    )
-                    ohe_masked_y = torch.unsqueeze(
-                        monai.networks.utils.one_hot(ys[j]*mask, num_classes=2, dim=0),
-                        axis=0
-                    )
-
-                    for metric in metrics:
-                        metric(y_pred=ohe_masked_pred, y=ohe_masked_y)
-                        logger.info("\t- {} = {}".format(type(metric).__name__, metric.aggregate().item()))
-                        metric.reset()
 
 
 if __name__ == "__main__":
@@ -195,4 +159,4 @@ if __name__ == "__main__":
 
     logger.debug(args)
 
-    main()
+    main(args.model, args.weights, args.schedule)
