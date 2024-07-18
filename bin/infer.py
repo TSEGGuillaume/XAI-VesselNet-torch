05bcdc047b9bfe4869b39acd558357f08aea1201
Guillaume Garret <Guillaume.GARRET@uca.fr>
2024-07-18 15:29:24 +0200

    Major improvement for inference functions  * Allow a pre-cropped patch to be directly infered  * Implement infer_patch() that crop the volume given a provided patch position and infer the patch  * Allow inference from a provided NIFTI file

--- bin/infer.py
+++ bin/infer.py
@@ -2,9 +2,40 @@ import argparse
 import logging
 import os
 
+import torch
+from torch.nn import Module
+
+from monai.data.meta_tensor import MetaTensor
+from monai.inferers import Inferer, SimpleInferer
+
+from monai.data import DataLoader, ArrayDataset
+from monai.transforms import (
+    Transform,
+    Compose,
+    Activations,
+    AsDiscrete,
+    RemoveSmallObjects,
+    SpatialCrop,
+    SpatialPad,
+)
+from monai.transforms import SaveImage
+
+from monai.inferers import SlidingWindowInferer
+from monai.data.utils import decollate_batch, first
+
+from models import instanciate_model
+from datasets.instanciate_dataset import instanciate_image_dataset
+from utils.prebuilt_logs import log_hardware
 from utils.load_hyperparameters import load_hyperparameters
 
-import models.instanciate_model
+from datasets.ImageDataset import ImageDatasetd
+from network.model_creator import init_inference_model
+
+logger = logging.getLogger("app")
+
+
+_CONST_BATCH_SIZE = 1  # `batch_size`==1 as data does not have equal spatial dims. See `sw_batch_size` variable instead
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -13,9 +44,9 @@ def parse_arguments():
         "model",
         type=str,
         metavar=("MODEL"),
-        choices=models.instanciate_model._all_models,
-        default=models.instanciate_model._all_models[0],
-        help="Path to the training CSV",
+        choices=instanciate_model._all_models,
+        default=instanciate_model._all_models[0],
+        help="Model name",
     )
     parser.add_argument(
         "weights",
@@ -24,18 +55,18 @@ def parse_arguments():
         help="Path to the model's weights",
     )
     parser.add_argument(
-        "csv_infer",
+        "data",
         type=str,
-        metavar=("INFER_CSV_PATH"),
-        help="Path to the inference CSV",
+        metavar=("DATA_PATH"),
+        help="Path to the data to infer (*.nii.gz, *.csv)",
     )
     parser.add_argument(
         "--hyperparameters",
         "-p",
         type=str,
-        metavar=("HYPERPARAMETERS_JSON_PATH"),
-        default=os.path.join(cfg.workspace, "resources", "default_hyperparameters.json"),
-        help="Path to the hyperparameters JSON",
+        metavar=("HYPERPARAM_PATH"),
+        default="./resources/default_hyperparameters.json",
+        help="Path to the hyperparameters file (*.json)",
     )
 
     parser.add_argument("--verbose", "-v", action="count", default=0)
@@ -44,88 +75,210 @@ def parse_arguments():
     return args
 
 
-def main():
-    import torch
-    from monai.data import DataLoader
-    from monai.inferers import SlidingWindowInferer
-    from monai.transforms import (
-        Compose,
-        Activations,
-        AsDiscrete,
-        RemoveSmallObjects,
-        SaveImage
-    )
-    from monai.data.utils import decollate_batch, first
+def infer_patch(
+    model: Module,
+    data: MetaTensor,
+    device: torch.device,
+    patch_pos: list,
+    input_size: tuple,
+    postprocessing: Transform = None,
+):
+    """
+    Create a patch from a provided spatial position and infer this patch.
+    The position of the patch must be a tuple list in the following format [ (start, ), (end, ) ].
+    The input size (patch size) is used to pad the patch if the end position of the roi exceed the image size.
+    To save the result of the inferences, compose the post-processing with a `SaveImage` object.
 
+    Args
+        model           : The trained model
+        data            : The image data
+        device          : The device to store the model
+        patch_pos       : The [ (start, ), (end, ) ] coordinate of the patch
+        input_size      : The input size (patch size)
+        postprocessing  : the post transforms to apply
 
-    from datasets.instanciate_dataset import instanciate_image_dataset
-    from models.instanciate_model import instanciate_model
-    from utils.prebuilt_logs import log_hardware
+    Returns
+        predictions (list[MetaTensor]) : The list of infered patch.
+    """
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    log_hardware(device)
+    if isinstance(data, MetaTensor) == False:
+        raise TypeError(f"Expected `MetaTensor`, got {type(data)}")
 
-    hyperparameters = load_hyperparameters(args.hyperparameters)
+    # Pre-processing
+    start_roi, end_roi = patch_pos
 
-    infer_ds = instanciate_image_dataset(args.csv_infer, image_only=False)
-    infer_loader = DataLoader(infer_ds, batch_size=1, num_workers=0) # Note: batch_size must be equal to 1 because input tensors does not have equal spatial dims. See sw_batch_size instead
-
-    saver = SaveImage(
-        output_dir=cfg.result_dir,
-        output_ext=".nii",
-        output_postfix=f"seg_{model_id}",
-        resample=False,
-        separate_folder=False
-    )
-
-    # Post-processing
-    post_transforms = Compose(
+    pre_T_composer = Compose(
         [
-            Activations(sigmoid=True),
-            AsDiscrete(threshold=0.5),
-            RemoveSmallObjects()
+            # Crop the volume to patch at roi position, and pad the patch if patch size is inferior to input size.
+            SpatialCrop(roi_start=start_roi, roi_end=end_roi),
+            SpatialPad(spatial_size=input_size),
         ]
     )
 
-    # Model
-    in_channels     = hyperparameters["in_channels"]
-    out_channels    = hyperparameters["out_channels"]
-    logger.debug(f"Input channels : {in_channels} | Output channels : {out_channels}")
+    # Manage the data
+    logger.debug("MetaTensor provided. Collate the data through a DataLoader...")
+    _ds = ArrayDataset(img=[data], img_transform=pre_T_composer)
+    data = DataLoader(_ds, batch_size=_CONST_BATCH_SIZE, num_workers=0)
 
-    model = instanciate_model(
-        args.model,
-        spatial_dims=3,
-        in_channels=in_channels,
-        out_channels=out_channels
-    ).to(device)
+    inferer = SimpleInferer()
 
-    model.load_state_dict(torch.load(args.weights)["model_state_dict"])
-    model.eval()
-
-    # Inference parameters
-    sw_batch_size   = hyperparameters["batch_size"]
-    sw_shape        = hyperparameters["patch_size"]
-    sw_overlap      = hyperparameters["patch_overlap"]
-
-    inferer = SlidingWindowInferer(
-        sw_shape,
-        sw_batch_size=sw_batch_size,
-        overlap=sw_overlap
+    predictions = infer(
+        model=model,
+        data=data,
+        device=device,
+        inferer=inferer,
+        postprocessing=postprocessing,
     )
 
-    for data in infer_loader:
-        x, meta = data["img"].to(device), data["img_meta"]
+    return predictions
 
-        pred = inferer(inputs=x, network=model)
-        pred = [post_transforms(i) for i in decollate_batch(pred)]
 
-        idx = 0
-        for prediction in pred:
-            
-            saver(prediction)
+def infer(
+    model: Module,
+    data: DataLoader,
+    device: torch.device,
+    inferer: Inferer = None,
+    postprocessing: Transform = None,
+) -> list[MetaTensor]:
+    """
+    Infer the data included in the data loader.
+    The `DataLoader` should be constructed from a `ImageDatasetd` or `ArrayDataset` dataset type. Otherwise, an exception is thrown.
+    If a patch-based model is used, infer the entire volume by supplying a `SlindingWindowInferer`, configured accordingly, as inferer.
+    To infer a single data / a single patch, provide a `SimpleInferer` as inferer. If inferer is None, `SimpleInferer` is used as default.
+    To save the result of the inferences, compose the post-processing with a `SaveImage` object.
 
-            logger.info("Save prediction for {}".format(os.path.basename(meta["filename_or_obj"][idx])))
-            idx+=1
+    Args:
+        model           : The trained model
+        data            : The data loader
+        device          : The device to store the model
+        inferer         : The inferer to use. `None` as default
+        postprocessing  : the post transforms to apply
+
+    Returns:
+        predictions (list[MetaTensor]) : The list of infered data.
+    """
+
+    if isinstance(data, DataLoader) == False:
+        raise TypeError(f"Expected data type `DataLoader`, got {type(data)}")
+
+    # Default behaviour
+    if inferer is None:
+        inferer = SimpleInferer()
+
+    if postprocessing is None:
+        postprocessing = Compose([])
+
+    # Prediction
+    predictions = []
+
+    if isinstance(data.dataset, ImageDatasetd):
+
+        for item in data:
+            x = item["img"].to(device)
+
+            logger.debug(f"Infer a {x.shape} tensor.")
+
+            y = inferer(inputs=x, network=model)
+            prediction = torch.stack([postprocessing(i) for i in decollate_batch(y)])
+
+            predictions.append(prediction)
+
+    elif isinstance(data.dataset, ArrayDataset):
+
+        for item in data:
+
+            if isinstance(item, MetaTensor) == False:
+                # If this exception is thrown, check the dataset construction. Unexpected segmentation data may have been provided
+                raise TypeError(f"Expected data type `MetaTensor`, got {type(item)}")
+
+            x = item.to(device)
+
+            logger.debug(f"Infer a {x.shape} tensor.")
+
+            y = inferer(inputs=x, network=model)
+            prediction = torch.stack([postprocessing(i) for i in decollate_batch(y)])
+
+            predictions.append(prediction)
+    else:
+        raise TypeError(
+            f"The `DataLoader` should be constructed from a `ImageDatasetd` or `ArrayDataset`."
+        )
+
+    return predictions
+
+
+def main():
+    # Select the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    log_hardware(device)
+
+    # Define variables from user args
+    model_name = args.model
+    weigths_path = args.weights
+    dataset_path = args.data
+
+    # Load hyperparameters for training
+    hyperparameters = load_hyperparameters(args.hyperparameters)
+
+    in_channels = hyperparameters["in_channels"]
+    out_channels = hyperparameters["out_channels"]
+
+    # Load the trained model
+    model = init_inference_model(
+        model_name=model_name,
+        weigths_path=weigths_path,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        device=device,
+    )
+
+    # Load the data to infer
+    infer_ds = instanciate_image_dataset(dataset_path, image_only=False)
+    infer_loader = DataLoader(infer_ds, batch_size=_CONST_BATCH_SIZE, num_workers=0)
+
+    # Verify that the provided `in_channels` in the setting file matches the actual data channels
+    # Assume the same input channels through the whole dataset
+    assert (
+        in_channels == first(infer_loader)["img"].shape[1]
+    ), "Provided `in_channels` in hyperparamaeters file does not match the actual image channel"
+
+    # Prepare the inferer
+    sw_batch_size = hyperparameters["batch_size"]
+    sw_shape = hyperparameters["patch_size"]
+    sw_overlap = hyperparameters["patch_overlap"]
+
+    inferer = SlidingWindowInferer(
+        sw_shape, sw_batch_size=sw_batch_size, overlap=sw_overlap
+    )
+
+    # To save the image. Deported from the inference function to allow customization
+    save_seg = SaveImage(
+        output_dir=os.path.join(cfg.result_dir, "inferences"),
+        output_ext=".nii",
+        output_postfix=f"seg_{model_id}",
+        resample=False,
+        separate_folder=False,
+    )
+
+    transforms = Compose(
+        [
+            Activations(sigmoid=True),
+            AsDiscrete(threshold=0.5),
+            RemoveSmallObjects(),
+            save_seg,
+        ]
+    )
+
+    predictions = infer(
+        model=model,
+        data=infer_loader,
+        inferer=inferer,
+        device=device,
+        postprocessing=transforms,
+    )
+
+    logger.info(f"End prediction for {len(predictions)} data.")
+    logger.info(f"Save directory : {save_seg.folder_layout.output_dir}")
 
 
 if __name__ == "__main__":
