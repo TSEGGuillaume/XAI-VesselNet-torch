a019bd111711b4fe38a6434a57bf17fe5fde9ec4
Guillaume Garret <Guillaume.GARRET@uca.fr>
2024-07-18 10:34:39 +0200

    Minor improvements  - train.py : comments and return typing  - instanciate_dataset.py : move import in global scope, typing, comments, format with Black  - dataset_reader.py : comments, typing  - load_hyperparameters : comments, typing

--- xai-vesselnet/utils/load_hyperparameters.py
+++ xai-vesselnet/utils/load_hyperparameters.py
@@ -6,8 +6,17 @@ import json
 logger = logging.getLogger("app")
 
 
-def load_hyperparameters(json_path: str):
+def load_hyperparameters(json_path: str) -> dict:
+    """
+    Load hyperparameters from a JSON file.
+    See default_hyperparameters.json for the file structure
 
+    Args:
+        json_path   : Path to the JSON file
+
+    Returns:
+        hyperparameters_cfg (dict) : The dictionnary containing hyperparameters
+    """
     with open(json_path, "r") as f:
         hyperparameters_cfg = json.load(f)
 
