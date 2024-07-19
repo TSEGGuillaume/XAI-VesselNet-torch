5ff6a6ac61d8fbd1f5416e93214bf6e6f47f4b1e
Guillaume Garret <Guillaume.GARRET@uca.fr>
2024-07-19 16:45:08 +0200

    Adapt evaluation to one-hot encoded data (in case of OHE training)  - Dynamic activation function  - Encode to one hot only for single channel data

--- bin/eval.py
+++ bin/eval.py
@@ -264,7 +264,7 @@ def main():
     # Post-transform
     transforms = Compose(
         [
-            Activations(sigmoid=True),
+            Activations(sigmoid=True) if out_channels == 1 else Activations(softmax=True),
             AsDiscrete(threshold=0.5),
             # RemoveSmallObjects(),
             save_seg,
@@ -285,11 +285,22 @@ def main():
     # 2/ Encode to One-Hot
     _mem_ys_pred = ys_pred
     ys_pred = []
-    for batch_pred in _mem_ys_pred:
-        ys_pred += [
-            OneHotEncoding(torch.unsqueeze(tensor, axis=0), num_classes=2, dim=1)
-            for tensor in decollate_batch(batch_pred)
-        ]
+
+    if out_channels > 1: 
+        # We assume ys_pred elements are in one-hot format
+        for batch_pred in _mem_ys_pred:
+            ys_pred += [
+                torch.unsqueeze(tensor, axis=0) for tensor in decollate_batch(batch_pred)
+            ]
+    elif out_channels == 1 :
+        # We assume ys_pred elements are in single-channel class indices
+        for batch_pred in _mem_ys_pred:
+            ys_pred += [
+                OneHotEncoding(torch.unsqueeze(tensor, axis=0), num_classes=2, dim=1)
+                for tensor in decollate_batch(batch_pred)
+            ]
+    else:  
+        raise RuntimeError("Tensors must have a channel dimension.")
 
     # Prepare the ground-truth data
     # 1/ Get the ground-truth in the right sample order given the loader
