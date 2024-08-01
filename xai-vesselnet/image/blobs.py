292fa9fd7cd5f150b555c5ec087e64098a3b7f7c
Guillaume Garret <Guillaume.GARRET@uca.fr>
2024-08-01 15:08:01 +0200

    Changes blobs pipeline  - Compute blob properties only on a selection of properties  - Lower the threshold in frangi filter -> more sensitive to blob-like structures  - Other minor changes

--- xai-vesselnet/image/blobs.py
+++ xai-vesselnet/image/blobs.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy import ndarray
 
 from skimage.filters import frangi, threshold_otsu
-from skimage.measure import label, regionprops
+from skimage.measure import label, regionprops_table
 
 
 logger = logging.getLogger("app")
@@ -40,7 +40,7 @@ def detect_blob(
         )
     )
 
-    frangi_beta = 0.95  # Sensitivity to deviation from a blob-like structure
+    frangi_beta = 0.5  # Sensitivity to deviation from a blob-like structure
     frangi_alpha = (
         1 - frangi_beta
     )  # Sensitivity to deviation from a plate-like structure
@@ -68,13 +68,14 @@ def detect_blob(
     return I_blob
 
 
-def compute_blobs_properties(I: ndarray) -> list:
+def compute_blobs_properties(I: ndarray, selected_props: list[str]) -> list:
     """
-    Labelized an image and measure various properties of the connected components.
+    Labelized an image and measure various selected properties of the connected components.
     See https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.regionprops
 
     Args:
         I : The image (H,W,[D])
+        selected_props  : The properties to compute for each labeled blob
 
     Returns:
         props (list) : The list of RegionProperties
@@ -83,14 +84,60 @@ def compute_blobs_properties(I: ndarray) -> list:
 
     logger.info(f"{nlabels} blobs detected")
 
+    props = []
+
     if nlabels == 0:
-        props = []
         logger.debug("No blob detected, returns empty region properties.")
     else:
-        # For debugging
         for lbl_idx in range(1, nlabels + 1):
             logger.debug(f" - Blob {lbl_idx}: {np.sum(labeled_blobs==lbl_idx)}")
 
-        props = regionprops(labeled_blobs)
+            current_blob = (labeled_blobs == lbl_idx).astype(np.ubyte)
 
-    return props
+            try:
+                current_props = regionprops_table(
+                    current_blob, properties=selected_props
+                )
+
+                # We use `regionprops_table` instead of `regionprops` because `regionprops_table` is executed when called, while `regionprops` is executed when indexed, causing exceptions for some small blobs (or flat simplex ?) during convex hull calculation.
+                # `regionprops_table` returns a dictionary of lists, where keys are the props and values are the corresponding property values for each blob.
+                # To match the output of `regionprops`, we convert the dictionary of lists to a list of dictionaries #### TODO and perform some post-processing, such as merging d["k-0"], d["k-1"], and d["k-2"] into d[k] = (d["k-0"], d["k-1"], d["k-2"])."
+
+                current_props = [
+                    dict(zip(current_props, t)) for t in zip(*current_props.values())
+                ]  # Change the dict of list to list of dict
+                props += current_props
+
+            except ValueError as e:
+
+                # Assume the problem came from the convex hull
+                if e.__str__() == "Surface level must be within volume data range.":
+                    problematic_props = [
+                        "area_convex",
+                        "feret_diameter_max",
+                        "image_convex",
+                        "solidity",
+                    ]
+
+                    # Remove from selected_props the problematic props
+                    intersect = set(problematic_props) & set(selected_props)
+
+                    corrected_props = selected_props.copy()
+                    for elem in intersect:
+                        corrected_props.remove(elem)
+
+                    current_props = regionprops_table(
+                        current_blob, properties=corrected_props
+                    )
+
+                    current_props = [
+                        dict(zip(current_props, t))
+                        for t in zip(*current_props.values())
+                    ]  # Change the dict of list to list of dict
+
+                    for elem in intersect:
+                        current_props[-1][elem] = -1.0
+
+                    props += current_props
+
+    return props, labeled_blobs, nlabels
