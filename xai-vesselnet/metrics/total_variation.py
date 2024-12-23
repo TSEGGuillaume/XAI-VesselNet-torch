e51bbafe9f8a3d4a74f7581fc1c41415eabc9864
Guillaume Garret <Guillaume.GARRET@uca.fr>
2024-12-23 09:28:04 +0100

    Implement mask management for total variation

--- xai-vesselnet/metrics/total_variation.py
+++ xai-vesselnet/metrics/total_variation.py
@@ -1,16 +1,20 @@
 import numpy as np
 from numpy import ndarray
 
-def image_total_variation(image:ndarray, neighborhood:str="N6", norm:str="L1") -> float:
+def image_total_variation(image:ndarray, neighborhood:str="N6", norm:str="L1", mask=None) -> float:
     """
     Compute the total variation of a 3D image, using L1 norm.
+    Note that this algorithm does not take into account image borders. Calculation starts at position corresponding to the radius of the structuring element.
+    TODO: implement border management using padding  
 
     Args:
         image                       : The image. Expected shape is [H,W,D].
         neighborhood (str, optional): The neighborhood to use ("N6" or "N26"). Defaults to "N6".
         norm (str, optional)        : The norm to use ("L1", "L2", "SQL2"). Defaults to "L1".
+        mask (ndarray, optional)    : The mask on which to compute the total variation. Same shape as image. If None, entire image is considered. Defaults to None.
 
     Raises:
+        AssertionError: if the image shape is not [H,W,D].
         ValueError: if the selected neighborhood is not available.
         ValueError: if the selected norm is not available.
 
@@ -18,6 +22,9 @@ def image_total_variation(image:ndarray, neighborhood:str="N6", norm:str="L1") -
         float: the total variation TV
     """
     assert len(image.shape) == 3, f"Expected shape for image (H,W,D), got ({image.shape})."
+
+    if mask is None:
+        mask = np.ones_like(image) # Consider every voxels
 
     SE_shape = (3,3,3)
 
@@ -30,7 +37,7 @@ def image_total_variation(image:ndarray, neighborhood:str="N6", norm:str="L1") -
         SE = np.ones(SE_shape)
     else:
         raise ValueError(f"{neighborhood} does not exist. Available neighborhood are : `N6` or `N26`")
-    
+
     # Plan to implement other norm types
     if norm == "L1":
         norm_fun = lambda matrix: np.sum(np.absolute(matrix))
@@ -42,13 +49,17 @@ def image_total_variation(image:ndarray, neighborhood:str="N6", norm:str="L1") -
         raise ValueError(f"{norm} norm type does not exist. Available norm are : `L1`, `L2` or `SQL2`")
     
     view = np.lib.stride_tricks.sliding_window_view(image, SE_shape)
+    mask_view = np.lib.stride_tricks.sliding_window_view(mask, SE_shape)
+
     # Flatten the first 3 dimensions to give a list of windows instead of a "grid" of windows
     # (grid_X, grid_Y, grid_Z, SE_shape_X, SE_shape_Y, SE_shape_Z) -> (n_windows, SE_shape_X, SE_shape_Y, SE_shape_Z)
     view = view.reshape( ( np.prod( view.shape[:-len(SE_shape)] ), ) + SE_shape )
+    mask_view = mask_view.reshape( ( np.prod( mask_view.shape[:-len(SE_shape)] ), ) + SE_shape )
 
     sum_variation = 0
-    for sub_image in view:
+    for sub_image, sub_mask in zip(view, mask_view):
         center_val = sub_image[1, 1, 1]
-        sum_variation += norm_fun(SE * center_val - SE * sub_image)
+        dynamic_neighbourhood = SE * sub_mask
+        sum_variation += norm_fun(dynamic_neighbourhood * center_val - dynamic_neighbourhood * sub_image)
 
     return sum_variation
