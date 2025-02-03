8c58095d4f2dc01e00dc0beceda7db5d0ec45e8a
Guillaume Garret <Guillaume.GARRET@uca.fr>
2025-02-03 15:32:00 +0100

    Improve the tubularity probability script  - Define a common multiprocessing framework to be used for other json generation scripts (blobs, thickness, ...) using Process and Queue.  - Implement a common progress bar for all processes, enabling better negotiation of remaining time.

--- bin/analyse_tubularity_probability.py
+++ bin/analyse_tubularity_probability.py
@@ -4,19 +4,19 @@ import logging.config # Mandatory if MONAI is not imported
 
 import os
 import json
-from multiprocessing import Process
+from multiprocessing import Queue, Process, cpu_count
+from queue import Empty
 
 from tqdm import tqdm
 
-import numpy as np
 from monai.transforms import LoadImage
 
 from graph.voreen_parser import voreen_VesselGraphSave_file_to_graph as LoadVesselGraph
 from utils.coordinates import anatomic_graph_to_image_graph as Anatomic2ImageGraph
-
 from utils.create_output_dirs import create_output_dir
 from utils.json_format import convert_typing_to_native
 from utils.get_landmark_from_args import get_landmark_obj as GetLandmark
+from utils.template_filename import CXAIVesselNetFilename as Filename
 
 logger = logging.getLogger("app")
 
@@ -55,111 +55,142 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+"""
+TODO
+We can continue the optimization by computing only once per patch as the tubularity props are found in the image given the landmark
+This does not really matter at the moment, the entire process is not so long
+
+In terms of writing, main() and distribute_tubularity_probs_json_creation() should be merged, unless distribute_tubularity_probs_json_creation() -> distribute_task_json_creation(), with distribute_task_json_creation a common thread manager for all called analysis processes (tubularity, blobs, ...) using Callback and kwargs as parameters.
+"""
+
+def pbar_listener(queue, ntasks):
+    pbar = tqdm(total=ntasks)
+    for _ in iter(queue.get, None):
+        pbar.update()
+
 
 def get_attribution_id(in_path):
     return os.path.normpath(in_path).split(os.sep)[-1]
 
 
-def task_compute_tubularity_prob(files, in_path_filters, in_path_graphs, out_path):
+def create_tubularity_probs_json(filename, graph, filters_map: dict, output_dir: str):
 
-    table_filters_id_name = {
-        "0000": "scan",
-        "0001": "frangi",
-        "0002": "jerman",
-        "0003": "sato",
-        "0004": "zhang",
-        "0005": "meijering",
-        "0006": "rorpo",
-    }
+    out_dict = {data["name"]: None for _, data in filters_map.items()}
 
-    graphs = {} # Will contain sample_id: graph_data ; graphs are common, whatever the filter used.
+    fname = Filename(filename=filename)
 
-    for id_filter, name_filter in table_filters_id_name.items():
+    for _, data_filter in filters_map.items():
+        I =  data_filter["image"]
 
-        Is = {} # Will contain sample_id: image_filter_data
+        landmark = GetLandmark(graph=graph, landmark_type=fname.landmark_type, landmark_id=fname.landmark_id)
 
-        logger.info(f"Processing filter {name_filter}")
-
-        for file in tqdm(files):
-
-            decomp_fname = file.split("_")
-
-            prefix_fname = "_".join(decomp_fname[:-2])
-            sample_id = "_".join(decomp_fname[:2])
-            landmark_type = decomp_fname[6]
-            landmark_id = decomp_fname[7]
-
-            if sample_id in Is.keys() and sample_id in graphs.keys():
-                I = Is[sample_id]
-                graph = graphs[sample_id]
-            else: # First time this sample is processed. Add to the dict.
-                I, meta = LoadImage(ensure_channel_first=False, image_only=False)(os.path.join(in_path_filters, f"{sample_id}_{id_filter}.nii.gz"))
-
-                graph = LoadVesselGraph(os.path.join(in_path_graphs, f"{sample_id}_graph.vvg"))
-                graph = Anatomic2ImageGraph(graph, meta["original_affine"])
-
-                Is[sample_id] = I
-                graphs[sample_id] = graph
-
-            landmark = GetLandmark(graph=graph, landmark_type=landmark_type, landmark_id=landmark_id)
-
-            vesselness_value = I[landmark.pos]
-
-            json_path = os.path.join(out_path, "{}_tubularity.json".format(prefix_fname))
-            
-            if os.path.isfile(json_path):
-                # UPDATE
-                with open(json_path, "r") as json_file:
-                    json_data = json.load(json_file)
-                    json_data.update({name_filter:vesselness_value})
-    
-                with open(json_path, "w") as json_file:
-                    json.dump(convert_typing_to_native(json_data), json_file, indent=4)
-            
-            else:
-                # FIRST TIME ; CREATE
-                with open(json_path, "w") as json_file:
-                    json.dump(convert_typing_to_native({name_filter:vesselness_value}), json_file, indent=4)
+        out_dict[data_filter["name"]] = I[landmark.pos]
     
 
-def main(in_path_attr, in_path_filters, in_path_graphs, out_path, threads_count=1):
-    # I/O
-    attribution_id = get_attribution_id(in_path_attr)
-    out_path = create_output_dir(
-        os.path.join(out_path, "tubularity"),
+    json_path = os.path.join(output_dir, "{}_tubularity.json".format(fname.get_prefix()))
+            
+    with open(json_path, "w") as json_file:
+        json.dump(convert_typing_to_native(out_dict), json_file, indent=4)
+
+
+def task_create_tubularity_probs_json(files_queue, files_finished_queue, graph, filters_map: dict, output_dir: str):
+    while True:
+        try:
+            task_file = files_queue.get()
+            
+            if task_file is None:
+                raise Empty
+
+        except Empty:
+            break
+
+        else:
+            # No exception raised, process the job and add the task completion to finished_queue
+            create_tubularity_probs_json(task_file, graph, filters_map, output_dir)
+            files_finished_queue.put(task_file)
+    
+    return True
+
+
+def distribute_tubularity_probs_json_creation(files: list[str], graph, map_data: dict, output_dir: str, process_count: None|int):
+    nfiles = len(files)
+
+    process_count = min(min(process_count, cpu_count()), nfiles)
+
+    files_to_process = Queue()
+    files_finished = Queue()
+
+    processes = []
+
+    for file in files:
+        files_to_process.put(file)
+
+    # Start the progress bar process
+    proc = Process(target=pbar_listener, args=(files_finished, nfiles))
+    proc.start()
+
+    # creating processes
+    for k in range(process_count):
+        files_to_process.put(None) # Stop condition
+        processes.append(
+            Process(target=task_create_tubularity_probs_json, args=(files_to_process, files_finished, graph, map_data, output_dir))
+        )
+        processes[-1].start()
+
+    # completing process
+    for p in processes:
+        p.join()
+
+    files_finished.put(None)
+    proc.join()
+
+
+def main(in_dir_attribution: str, in_dir_filters: str, in_dir_graphs: str, output_dir: str, process_count=None):
+
+    attribution_id = get_attribution_id(in_dir_attribution)
+    output_dir = create_output_dir(
+        os.path.join(output_dir, "tubularity"),
         attribution_id
     )
 
-    files = [f for f in os.listdir(in_path_attr) if f.endswith(".nii.gz")]
-    nfiles = len(files)
-    logger.info(f"Files count : {nfiles}")
+    map_data = {
+        "0000": { "name" : "scan", },
+        "0001": { "name" : "frangi", },
+        "0002": { "name" : "jerman", },
+        "0003": { "name" : "sato", },
+        "0004": { "name" : "zhang", },
+        "0005": { "name" : "meijering", },
+        "0006": { "name" : "rorpo", },
+    }
 
-    if nfiles > 0:
+    image_loader = LoadImage(ensure_channel_first=False, image_only=False)
 
-        if nfiles < threads_count:
-            threads_count = nfiles
-        
-        files_chunks = np.array_split(files, threads_count)
+    files = [f for f in os.listdir(in_dir_attribution) if f.endswith(".nii.gz")]
 
-        threads = []
-        for id_chunk, chunk in enumerate(files_chunks):
-            chunk = chunk.tolist()
+    unique_samples = { "_".join(file.split('_')[:2]) for file in files }
 
-            logger.debug(f"Chunk {id_chunk} : {len(chunk)} files.")
+    for sample in unique_samples:
+        logger.info(f"Process sample {sample}")
+        graph = LoadVesselGraph(os.path.join(in_dir_graphs, f"{sample}_graph.vvg"))
 
-            threads.append(
-                Process(target=task_compute_tubularity_prob, args=(chunk, in_path_filters, in_path_graphs, out_path))
-            )
-            threads[-1].start()
-            logger.info(f"Thread {id_chunk}: start")
+        mem_affine = None
 
-           
-        for thread_idx, thread in enumerate(threads):
-            thread.join()
-            logger.info(f"Thread {thread_idx}: finished")
+        for id_filter, _ in map_data.items():
+            image, meta = image_loader(os.path.join(in_dir_filters, f"{sample}_{id_filter}.nii.gz"))
 
-    else:
-        logger.error("No files found in the directory.")
+            if mem_affine is None:
+                mem_affine = meta["original_affine"]
+            else:
+                assert (mem_affine == meta["original_affine"]).all(), f"Affines don't match for sample {sample}"        
+
+            map_data[id_filter]["image"] = image
+            map_data[id_filter]["meta"] = meta
+
+        graph = Anatomic2ImageGraph(graph, mem_affine)
+
+        sample_files = [f for f in files if f.startswith(sample)]
+
+        distribute_tubularity_probs_json_creation(sample_files, graph, map_data, output_dir, process_count)
 
 
 if __name__ == "__main__":
@@ -177,9 +208,5 @@ if __name__ == "__main__":
         },
     )
     logger = logging.getLogger("app")
-
-    for handler in logger.handlers:
-        if type(handler) == logging.StreamHandler:
-            handler.setLevel(logging.ERROR)
 
     main(args.attributions_dir, args.filters_dir, args.graphs_dir, cfg.result_dir, args.thread)
